43b37c47d7e87ef6287cec090c176ed350479426
Ruka <kubotaruka3@gmail.com>
2025-04-17 02:37:37 +0000

    changeScript

--- Project/CreateSquare.py
+++ Project/CreateSquare.py
@@ -1,6 +1,14 @@
 from manim import *
 
-class CreateSquare(Scene):
+class CircleMove(Scene):
     def construct(self):
-        self.play(Create(Square()))
+        circle = Circle(color = BLUE)  # Mobject機能
+        circle.move_to([-2, 2, 0])     # Mobject機能
 
+        animFirst = Create(circle)     # Animation機能
+        animMiddle = circle.animate.move_to([-2, -2, 0]).set_color(GREEN)
+        animEnd = FadeOut(circle)      # Animation機能
+
+        self.play(animFirst)           # Scene機能
+        self.play(animMiddle)          # Scene機能
+        self.play(animEnd)             # Scene機能
